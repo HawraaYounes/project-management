29ec06876449ef6be8f4b71924582f5501710c44
HawraaYounes <hawraayounes0@gmail.com>
2024-06-11 11:46:18 +0300

    added modal

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import Sidebar from "./components/Sidebar";
+import AddProjectModal from "./components/AddProjectModal";
 
 function App() {
   return (
-    <>
+    <main className="h-screen my-8">
       <Sidebar/>
-    </>
+      <AddProjectModal />
+    </main>
   );
 }
 
