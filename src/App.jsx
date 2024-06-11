76e2958bc454b76878333bd8c6d360f25f119824
HawraaYounes <hawraayounes0@gmail.com>
2024-06-11 17:08:52 +0300

    managing state to switch btw components

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,33 @@
 import Sidebar from "./components/Sidebar";
 import AddProjectModal from "./components/AddProjectModal";
+import NoProjectSelected from "./components/NoProjectSelected";
+import { useState } from "react";
 
 function App() {
+  const [projectSelected, setProjectSelected] = useState({
+    projectId: undefined,
+    projects: [],
+  });
+
+  let content;
+  if (projectSelected.projectId === undefined) {
+    content = <NoProjectSelected onProjectAdd={onProjectAdd} />;
+  } else if (projectSelected.projectId === null) {
+    content = <AddProjectModal />;
+  }
+
+  function onProjectAdd() {
+    setProjectSelected((prevProjects) => {
+      return {
+        ...prevProjects,
+        projectId: null,
+      };
+    });
+  }
   return (
-    <main className="h-screen my-8">
-      <Sidebar/>
-      <AddProjectModal />
+    <main className="h-screen my-8 flex gap-8">
+      <Sidebar onProjectAdd={onProjectAdd}/>
+      {content}
     </main>
   );
 }
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,31 +1,24 @@
 import { useState } from "react";
+import Button from "./Button";
 
-function handleClick(){
+const Sidebar = ({onProjectAdd}) => {
 
-}
-
-function addProject(){
-
-}
-
-const Sidebar = () => {
-  const [projects, setProjects] = useState([]);
   return (  
     <aside className="w-1/3 px-8 py-16 bg-stone-900 text-stone-50 md:w-72 rounded-r-xl ">
       <h2 className="mb-8 font-bold uppercase md:text-xl text-stone-200">
         Your Projects
       </h2>
-      <button
+      <Button
         className="px-4 py-2 text-xs md:text-base rounded-md bg-stone-700 text-stone-400 hover:bg-stone-600 hover:text-stone-100"
-        onClick={handleClick}
+        onClick={onProjectAdd}
       >
         + Add Project
-      </button>
-      <ul className="mt-8">
+      </Button>
+      {/* <ul className="mt-8">
         {projects.map((project) => (
           <li className="flex justify-between my-4">{project.name}</li>
         ))}
-      </ul>
+      </ul> */}
     </aside>
   );
 };
