75d814a2ba8aa10d8b0474e0db951b71c31763f9
HawraaYounes <hawraayounes0@gmail.com>
2024-06-19 17:53:30 +0300

    handled delete task

--- src/components/Tasks.jsx
+++ src/components/Tasks.jsx
@@ -15,7 +15,7 @@ const Tasks = ({ tasks, onAddTask, onDeleteTask }) => {
           {tasks.map((task) => (
             <li key={task.id} className="flex justify-between my-4">
               <span>{task.text}</span>
-              <button className="text-stone-700 hover:text-red-500">
+              <button className="text-stone-700 hover:text-red-500" onClick={()=>onDeleteTask(task.id)}>
                 Clear
               </button>
             </li>
